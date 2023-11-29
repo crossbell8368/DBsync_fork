a7c57a38d521f8ff5dfd5d1d2d465b9635d327ab
crossbell8368 <godfather346@gmail.com>
2023-11-29 18:10:00 +0900

    [Config] Add SpringBoot Project Template & Git Setting * Project Template   * 패키지명 : com.todoPang   * 프로젝트명 : DBsync   * 자바버젼 : 17   * Gradle버젼 : 8.4   * 초기 프레임워크     * springboot-web     * MySQL Connector     * lombok * Git Setting   * .gitignore 추가   * .gitattributes 추가

---
+++ src/main/java/com/todoPang/DBsync/DBsyncApplication.java
@@ -0,0 +1,13 @@
+package com.todoPang.DBsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DBsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DBsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todoPang/DBsync/DBsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todoPang.DBsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DBsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
